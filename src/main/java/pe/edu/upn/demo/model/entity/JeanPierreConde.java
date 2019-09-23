890d26c578fc130a98c43657588cffcdf937d088
Jean Pierre-Conde <lccogu@gmail.com>
2019-09-23 16:41:40 -0500

    Enviado por Jean Pierre Conde

---
+++ src/main/java/pe/edu/upn/demo/model/entity/JeanPierreConde.java
@@ -0,0 +1,15 @@
+package pe.edu.upn.demo.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+
+public class JeanPierreConde {
+
+	private String id;
+	private String segundoNombre;
+	private String ApellidoMaterno;
+	
+	
+}
